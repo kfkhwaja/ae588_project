9d899f6446524001824c2937ee57b46b75ef0a86
Kabir Khwaja <khwaja@umich.edu>
2025-12-03 18:22:09 -0500

    add rosenbrock comp file

---
+++ tests/rosenbrock_comp.py
@@ -0,0 +1,7 @@
+# test script to compare OpenMDAO vs ASB performance on rosenbrock problem
+
+# try increasing dimension
+
+# compare iterations, wall time, function evaluations, etc.
+
+# maybe compare them to SciPy?
